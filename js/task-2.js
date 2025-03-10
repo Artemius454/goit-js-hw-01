459de3a30b4621cf967c43509777f5922389e568
Art1X <artemius454@gmail.com>
2025-03-10 21:40:26 +0300

    hw1

--- js/task-2.js
+++ js/task-2.js
@@ -4,7 +4,6 @@ function getShippingMessage(country, price, deliveryFee) {
     return message;
 }
 
-const message1 = getShippingMessage();
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
